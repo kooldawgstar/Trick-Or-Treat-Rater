4d41bad29d73588ec668eaed99407ee51ad7862f
kooldawgstar <svanikss@gmail.com>
2016-01-06 20:47:18 -0500

    Create key.php

---
+++ key.php
@@ -0,0 +1,94 @@
+<button type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="left" data-content=
+'<div>
+  <p>One Star</p>
+   <span>
+     <img src="toothbrush.png" style="width:100px;height:50px;"/>
+   </span>
+</div>
+
+<div>
+ <p>
+  Two Star
+ </p>
+  <span>
+   <img src="http://www.clipartbest.com/cliparts/dc8/MA4/dc8MA4bce.png" style="width:50px;height:50px;"/>
+   <img src="http://www.clipartbest.com/cliparts/dc8/MA4/dc8MA4bce.png" style="width:50px;height:50px;"/>
+  </span>
+</div>
+<div>
+<p>
+  Three Star
+ </p>
+  <span>
+   <img src="http://www.clipartbest.com/cliparts/dc8/MA4/dc8MA4bce.png" style="width:25px;height:25px;"/>
+   <img src="http://www.clipartbest.com/cliparts/dc8/MA4/dc8MA4bce.png" style="width:25px;height:25px;"/>
+   <img src="http://www.clipartbest.com/cliparts/dc8/MA4/dc8MA4bce.png" style="width:25px;height:25px;"/>
+  </span>
+</div>
+
+<div>
+<p>
+  Four Star
+ </p>
+  <span>
+   <img src="http://www.clipartbest.com/cliparts/dc8/MA4/dc8MA4bce.png" style="width:25px;height:25px;"/>
+   <img src="http://www.clipartbest.com/cliparts/dc8/MA4/dc8MA4bce.png" style="width:25px;height:25px;"/>
+   <img src="http://www.clipartbest.com/cliparts/dc8/MA4/dc8MA4bce.png" style="width:25px;height:25px;"/>
+   <img src="http://www.clipartbest.com/cliparts/dc8/MA4/dc8MA4bce.png" style="width:25px;height:25px;"/>
+  </span>
+</div>
+
+<div>
+<p>
+  Five Star
+ </p>
+  <span>
+   <img src="http://www.clipartbest.com/cliparts/dc8/MA4/dc8MA4bce.png" style="width:25px;height:25px;"/>
+   <img src="http://www.clipartbest.com/cliparts/dc8/MA4/dc8MA4bce.png" style="width:25px;height:25px;"/>
+   <img src="http://www.clipartbest.com/cliparts/dc8/MA4/dc8MA4bce.png" style="width:25px;height:25px;"/>
+   <img src="http://www.clipartbest.com/cliparts/dc8/MA4/dc8MA4bce.png" style="width:25px;height:25px;"/>
+   <img src="http://www.clipartbest.com/cliparts/dc8/MA4/dc8MA4bce.png" style="width:25px;height:25px;"/>
+  </span>
+</div>
+
+
+<style>
+#key{
+  font-family: sans-serif;
+  color: black;
+  display: inline-block;
+  font-weight: 100;
+  text-align: center;
+  font-size: 30px;
+}
+#key > div{
+  padding: 10px;
+  border-radius: 3px;
+  background: yellow;
+  display: inline-block;
+  margin:10px;
+}
+#key div > span{
+  padding: 15px;
+  border-radius: 3px;
+  background: black;
+  color: white;
+  display: inline-block;
+}
+
+#key .smalltext{
+  padding-top: 5px;
+  font-size: 16px;
+}
+</style>' data-html="true">
+Learn our site's symbols</button>
+<style>
+.extra-info h2 {
+    width: 100px;
+    background-color: yellow;
+    border-radius: 5px;
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
