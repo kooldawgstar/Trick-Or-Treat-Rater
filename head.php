1711c0e07a227f7d71e4b31a1c5b4c2b6f402225
kooldawgstar <svanikss@gmail.com>
2016-01-06 20:46:23 -0500

    Create head.php

---
+++ head.php
@@ -0,0 +1,10 @@
+<head>
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="theme.css">
+  <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <script src="scripts.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo(isset($page_title) ? $page_title : "Trick or Treat Rater"); ?></title>
+  <?php echo(isset($additional_head_code) ? $additional_head_code : ""); ?>
+</head>
